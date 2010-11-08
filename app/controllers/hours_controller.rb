d649345b02a63061d8eb782dbe85a470743ed5ac
deb <deb@deb-desktop.(none)>
2010-11-08 02:47:54 +0100

    get project information for hour management

--- app/controllers/hours_controller.rb
+++ app/controllers/hours_controller.rb
@@ -15,5 +15,36 @@ class HoursController < ApplicationController
     @year = params[:year].to_i
     @week = params[:week].to_i
     
+    @projects = get_projects(@year,@week)
   end
+  
+private
+  # function that gets all the projects with the jobs for an employee
+  #  in a certain week
+  def get_projects(year, week)
+    begin
+      emp_id  = session[:user_id]
+      week = week
+      year    = year
+      
+      # Projects where the employee is a manager or partner
+      p_partnerManager = Project.find_by_sql ["SELECT DISTINCT p.* FROM projects p WHERE partner_id = ? OR manager_id = ?", emp_id, emp_id]
+  
+      # Projects where the employee has a job assigned
+      p_gotJobAssigned = Project.find_by_sql ["SELECT DISTINCT p.* FROM projects p, jobs j WHERE p.id=j.project_id AND j.employee_id=?", emp_id]
+  
+      # We merge the two into one, skipping duplicates
+      projects = p_partnerManager | p_gotJobAssigned  
+  
+      # We filter the projects by date
+      week_start = Date.commercial(year, week, 1)
+      week_end   = Date.commercial(year, week, 5)
+      projects.reject! {|p| p.date_start > week_end or p.date_end < week_start}
+
+      return projects
+
+    rescue Exception => e
+      logger.error { "Error [hours_controller.rb/get_projects] #{e.message}" }
+    end
+  end  
 end
