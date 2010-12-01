1b9c3fce153da71546d24dbab57fac86b61bb566
Jorge Yau <jorgeyau@gmail.com>
2010-12-01 16:00:45 -0500

    Added basic styles

---
+++ public/javascripts/dropdown.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$("ul.dropdown li").dropdown();
+});
+
+$.fn.dropdown = function() {
+	$(this).hover(function(){
+		$(this).addClass("hover");
+		$('> .dir',this).addClass("open");
+		$('ul:first',this).css('display', 'block');
+	},function(){
+		$(this).removeClass("hover");
+		$('.open',this).removeClass("open");
+		$('ul:first',this).css('display', 'none');
+	});
+
+}
