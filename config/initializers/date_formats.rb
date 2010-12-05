66a418d789faf4df2ba43924edfc3bbf77633d73
Jorge Yau <jorgeyau@gmail.com>
2010-12-04 23:22:10 -0500

    Styled employee page

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -2,5 +2,6 @@ ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
  :date => '%d/%m/%Y',
  :date_time12  => "%d/%m/%Y %I:%M%p",
  :date_time24  => "%d/%m/%Y %H:%M",
- :full_date => "%A, %B %d, %Y"
+ :full_date => "%A, %B %d, %Y",
+ :short_date => "%B %d, %Y"
 )
