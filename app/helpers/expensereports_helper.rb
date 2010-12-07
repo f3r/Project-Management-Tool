5c2e45cf5b1d40b57514378b23166c6236da5f70
Jorge Yau <jorgeyau@gmail.com>
2010-12-06 22:30:03 -0500

    Added demo graph for expense reports

--- app/helpers/expensereports_helper.rb
+++ app/helpers/expensereports_helper.rb
@@ -1,2 +1,15 @@
 module ExpensereportsHelper
+
+  def expense_chart(start_time)
+    expenses_by_day = Expensereport.find( :all, 
+                                          :conditions => {:expenseDate => start_time.beginning_of_day..Time.zone.now.end_of_day}, 
+                                          :group => "date(expenseDate)", 
+                                          :select => "expenseDate, sum(amount) as total_amount"
+                                          )
+    (start_time.to_date..Date.today).map do |date|
+      expense = expenses_by_day.detect { |expense| expense.expenseDate.to_date == date }
+      expense && expense.total_amount.to_f || 0
+    end.inspect
+  end
+
 end
