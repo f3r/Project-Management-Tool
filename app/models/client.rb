35b69702b3adde9c1ab47e1138cb2d3e50770f72
Jorge Yau <jorgeyau@gmail.com>
2010-12-07 22:39:41 -0500

    Added validations to client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,6 @@
 class Client < ActiveRecord::Base
   has_many :projects, :dependent => :destroy
+  validates_presence_of   :name, :cif, :email
+  validates_format_of     :email, :with => /^[A-Z0-9.+_%-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i
+  validates_uniqueness_of :email, :cif
 end
